39d0d4e543259f973f0f859322a4ceea3905ab45
makicamel <unright@gmail.com>
2022-01-02 15:43:22 +0900

    Clean up comments

--- config/application.rb
+++ config/application.rb
@@ -20,20 +20,8 @@ Bundler.require(*Rails.groups)
 
 module MonthlyDiary
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
-
     config.generators do |g|
       g.javascripts false
       g.helper false
